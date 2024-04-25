6ff253f6c9bcecf39a3be2da433e7e7079a64a6b
maaz <maaz1628@gmail.com>
2024-04-26 00:51:54 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Practical react essentials
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='mb-4'>
+        Hello World!
+        </h1>
+      <p>
+        Maaz iqbal
+        </p>
+    </>
+    
+  )
+}
+
+export default App
